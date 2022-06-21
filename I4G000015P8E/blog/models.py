f9320e87c83c030eae2d34ef0ddcbc4eaa592651
abiketechie <abikeogunmola@gmail.com>
2022-06-21 01:29:22 +0100

    initial commit

---
+++ I4G000015P8E/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.db.models import Model
+from django.contrib.auth import get_user_model
+User=get_user_model()
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField(max_length=200)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField('date created')
+    Published_date = models.DateTimeField('date published')
